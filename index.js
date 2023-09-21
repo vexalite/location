1a0a2d569cd75446a8800dc9dde9ac5f6d2de085
vexalite <125900539+vexalite@users.noreply.github.com>
2023-09-21 13:24:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// Create a Redis stream for a user
+
+
 app.post('/api/user/stream', async (req, res) => {
     try {
       const { userId } = req.params;
@@ -37,21 +38,16 @@ app.post('/api/user/stream', async (req, res) => {
     }
   });
   
-// Add data to a user's stream
 app.post('/api/user/:streamName', async (req, res) => {
     try {
       const { userId, streamName } = req.params;
-      const latestCoordinates  = req.body;
-
-    // console.log(latestCoordinates)
-    await redisClient.xadd(streamName, '*','data', JSON.stringify(req.body));
-    // const { req.body } = latestCoordinates
-    //   medianCoordinates.forEach((medianCoord, index) => {
-    //     const distance = geolib.getDistance(latestCoordinates, medianCoord);
-    //     if (distance <= 200) {
-    //         console.log(`Alert`)
-    //     }
-    // });
+      
+      const payload = {
+        data: req.body, // Assuming req.body contains the data you want to include
+      };
+    
+    await redisClient.xadd(streamName, '*', 'data', JSON.stringify(payload));
+    
       res.status(201).json({ message: 'Data added to the stream' });
     } catch (error) {
       console.error('Error adding data to user stream:', error);
@@ -59,24 +55,61 @@ app.post('/api/user/:streamName', async (req, res) => {
     }
   });
   
-// Query data from a user's stream
+
 app.get('/api/user/:streamName', async (req, res) => {
     try {
       const { userId, streamName } = req.params;
       
       const streamData = await redisClient.xrange(streamName, '-', '+');
       
-      const extractedData = streamData.map(([messageId, message]) => message)
+      const extractedData = streamData.map(([messageId, message]) => JSON.parse(message[1]))
         
-        // const data = JSON.parse(message.data)
-
-      //   return {
-      //     message
-      //   };
-      // });
       res.status(200).json(extractedData);
     } catch (error) {
       console.error('Error fetching data from user stream:', error);
       res.status(500).json({ error: 'Internal server error' });
     }
   });
+
+  app.post('api/notify', async (req,res) => {
+      
+    const { fcm_token, studentName } = req.body
+
+    const admin = require('firebase-admin');
+
+// Initialize Firebase Admin SDK with your service account credentials
+const serviceAccount = require('./path-to-your-service-account-key.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// The FCM token of the target device
+const targetDeviceToken = 'TARGET_DEVICE_FCM_TOKEN';
+
+const message = {
+  notification: {
+    title: 'Notification Title',
+    body: 'Notification Body',
+  },
+  token: targetDeviceToken,
+};
+
+admin.messaging().send(message)
+  .then((response) => {
+    console.log('Notification sent successfully:', response);
+    // Handle success
+  })
+  .catch((error) => {
+    console.error('Error sending notification:', error);
+    // Handle error
+  });
+
+
+
+    const payload = {
+      data: req.body, 
+    };
+
+    await redisClient.xadd(streamName, '*', 'data', JSON.stringify(payload))
+
+  })
